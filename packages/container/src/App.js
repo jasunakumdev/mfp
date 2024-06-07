041cb9a793bed11f6f3ce44f2e7bdd0f6b16c88a
Jasu Nakum <jasunakum@Jasus-MacBook-Pro.local>
2024-06-07 10:29:59 +0530

    Trigger action 4

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import MarketingApp from './components/MarketingApp'
-//new app sample test comment
+//new app sample test comment added
 export default () => {
   return (
     <div>
